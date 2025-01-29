9c39273602293900508bf9b1f7b1bb9ca0ed123e
Hunter Poole <punterhoole@gmail.com>
2025-01-28 21:09:00 -0500

    H2: Complete Problem 4
    Wrote and tested Problem 4 solution.
    
    Problem 4:
    
    Workers at a company have won a 7.6% annual pay increase, which will be retroactive for six months. Write an algorithm, then a program, that takes an employee’s previous annual salary as input, and outputs the amount of retroactive pay due to the employee, the new annual salary, and the new monthly salary. Test program with the following three salaries of $35,000, $46,500, and $52,450. Output from program should look close to following and should have $ sign as well:
    
    Please enter employee's salary --> 35000
    Retroactive Pay --> $1330.0
    New Annual Salary --> $37660.0
    New Monthly Salary --> $3138.3333

---
+++ HW2/src/Problem_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class Problem_4 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner Base_Annual_Salary_Input = new Scanner(System.in);
+        System.out.print("Please enter employee's salary --> ");
+        float Base_Annual_Salary = Base_Annual_Salary_Input.nextFloat();
+        Base_Annual_Salary_Input.close();
+
+        float Retroactive_Pay = ((Base_Annual_Salary / 2) * (float)1.076) - (Base_Annual_Salary / 2);
+        System.out.println("Retroactive Pay --> $" + Retroactive_Pay);
+
+        float Adjusted_Annual_Salary = Base_Annual_Salary * (float)1.076;
+        System.out.println("New Annual Salary --> $" + Adjusted_Annual_Salary);
+
+        float Adjusted_Monthly_Salary = Adjusted_Annual_Salary / 12;
+        System.out.println("New Monthly Salary --> $" + Adjusted_Monthly_Salary);
+    }
+}
