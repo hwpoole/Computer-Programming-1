04cd17e6b02e4fad677ea30ccf6dd327e418b5f4
Hunter Poole <punterhoole@gmail.com>
2025-01-30 19:09:07 -0500

    H2: Revert "Separate variable declarations and print statements."
    This reverts commit ca213c5c07b1ca455de4f3146a17288662276c7d.

--- HW2/src/Problem_4.java
+++ HW2/src/Problem_4.java
@@ -17,11 +17,12 @@ public class Problem_4 {
         float Base_Annual_Salary = Input.nextFloat();
 
         float Retroactive_Pay = ((Base_Annual_Salary / 2) * 1.076f) - (Base_Annual_Salary / 2);
-        float Adjusted_Annual_Salary = Base_Annual_Salary * 1.076f;
-        float Adjusted_Monthly_Salary = Adjusted_Annual_Salary / 12;
-
         System.out.println("Retroactive Pay --> $" + Retroactive_Pay);
+
+        float Adjusted_Annual_Salary = Base_Annual_Salary * 1.076f;
         System.out.println("New Annual Salary --> $" + Adjusted_Annual_Salary);
+
+        float Adjusted_Monthly_Salary = Adjusted_Annual_Salary / 12;
         System.out.println("New Monthly Salary --> $" + Adjusted_Monthly_Salary);
     }
 }
