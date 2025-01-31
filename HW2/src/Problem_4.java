7d14e6df2542fb4ef22da90c0a05549042c72db8
Hunter Poole <punterhoole@gmail.com>
2025-01-30 19:08:13 -0500

    H2: Separate variable declarations and print statements.
    Move variable declarations to the top underneath the input function.
    
    Condense print statements into one block.

--- HW2/src/Problem_4.java
+++ HW2/src/Problem_4.java
@@ -17,12 +17,11 @@ public class Problem_4 {
         float Base_Annual_Salary = Input.nextFloat();
 
         float Retroactive_Pay = ((Base_Annual_Salary / 2) * 1.076f) - (Base_Annual_Salary / 2);
-        System.out.println("Retroactive Pay --> $" + Retroactive_Pay);
-
         float Adjusted_Annual_Salary = Base_Annual_Salary * 1.076f;
-        System.out.println("New Annual Salary --> $" + Adjusted_Annual_Salary);
-
         float Adjusted_Monthly_Salary = Adjusted_Annual_Salary / 12;
+
+        System.out.println("Retroactive Pay --> $" + Retroactive_Pay);
+        System.out.println("New Annual Salary --> $" + Adjusted_Annual_Salary);
         System.out.println("New Monthly Salary --> $" + Adjusted_Monthly_Salary);
     }
 }
