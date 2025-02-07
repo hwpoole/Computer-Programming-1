f8046dc46ec2aac554a76bee4ee92caa9719263b
Hunter Poole <punterhoole@gmail.com>
2025-02-06 21:32:36 -0600

    H2: Change formatting, clean verbose terminal output

--- HW2/src/Problem_5.java
+++ HW2/src/Problem_5.java
@@ -11,13 +11,15 @@ import java.util.Scanner;
 
 public class Problem_5 {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
 
         Scanner Input = new Scanner(System.in);
         System.out.print("Please enter a year: ");
         int Year = Input.nextInt();
 
-        if (Year % 400 == 0) {
+        if (Year % 400 == 0)
+        {
             System.out.println(Year + " is a leap year.");
         } else if (Year % 100 != 0 && Year % 4 == 0) {
             System.out.println(Year + " is a leap year.");
@@ -28,28 +30,14 @@ public class Problem_5 {
 }
 
 /*
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>  & 'C:\Program Files\Java\jdk-23\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2\bin' 'Problem_5' 
-
 Please enter a year: 2000
 2000 is a leap year.
-
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2> ^C
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2> 
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>  c:; cd 'c:\Users\hunte\Documents\GitHub\155-W2-H2\HW2'; & 'C:\Program Files\Java\jdk-23\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2\bin' 'Problem_5' 
 
 Please enter a year: 2001
 2001 is not a leap year.
 
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2> ^C
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>  c:; cd 'c:\Users\hunte\Documents\GitHub\155-W2-H2\HW2'; & 'C:\Program Files\Java\jdk-23\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2\bin' 'Problem_5'        
-
 Please enter a year: 1988
 1988 is a leap year.
-
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2> ^C
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>
-PS C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2>  c:; cd 'c:\Users\hunte\Documents\GitHub\155-W2-H2\HW2'; & 'C:\Program Files\Java\jdk-23\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\hunte\Documents\GitHub\155-W2-H2\HW2\bin' 'Problem_5'        
 
 Please enter a year: 2031
 2031 is not a leap year.
