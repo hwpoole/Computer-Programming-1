36c3053339a72e79be117348288998f5d0278d0c
Hunter Poole <punterhoole@gmail.com>
2025-02-06 21:33:12 -0600

    H1: Update formatting/error handling
    Remove exception throwing. Update { formatting. Clean overly verbose output.

--- HW1P4_Code.java
+++ HW1P4_Code.java
@@ -10,7 +10,8 @@ Action: Given two numbers, returns them in order of the larger one first.
 import java.util.Scanner;
 
 public class HW1P4_Code {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)
+    {
 
         Scanner Num1_Input = new Scanner(System.in);
         System.out.print("Enter the first number: ");
@@ -23,7 +24,8 @@ public class HW1P4_Code {
         Num1_Input.close();
         Num2_Input.close();
 
-        if (Num1 > Num2) {
+        if (Num1 > Num2)
+        {
             System.out.println("The two numbers are: " + Num1 + ", " + Num2);
         } else {
             System.out.println("The two numbers are: " + Num2 + ", " + Num1);
@@ -31,23 +33,11 @@ public class HW1P4_Code {
     }
 }
 
-/* I am using VSCode, output below is the full terminal output */
-
 /*
-hunterpoole@Megs-MacBook-Pro Documents %  /usr/bin/env /Library/Java/JavaVirtualMachines/t
-emurin-21.jdk/Contents/Home/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /Users/hu
-nterpoole/Library/Application\ Support/Code/User/workspaceStorage/aa4af2d5ea8777f0c20fee4a
-a25f3c30/redhat.java/jdt_ws/Documents_5c3529dc/bin HW1P4_Code 
 
 Enter the first number: 15
 Enter the second number: 44
 The two numbers are: 44, 15
-
-hunterpoole@Megs-MacBook-Pro Documents %  cd /Users/hunterpoole/Documents ; /usr/bin/env /
-Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/java -XX:+ShowCodeDetail
-sInExceptionMessages -cp /Users/hunterpoole/Library/Application\ Support/Code/User/workspa
-ceStorage/aa4af2d5ea8777f0c20fee4aa25f3c30/redhat.java/jdt_ws/Documents_5c3529dc/bin HW1P4
-_Code 
 
 Enter the first number: 32
 Enter the second number: 21
