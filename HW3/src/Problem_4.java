a87a82c2019a57fcd6d9cf4ea4af5b9c1e790757
Hunter Poole <punterhoole@gmail.com>
2025-02-10 14:02:11 -0500

    H3: Declare variables - Income, Tax, MaxTax5p
    Declare variables to be used.
    
    State Tax rate schedule is divided into three buckets:
    
    0 - 15000 @ 0%
    15001 - 30000 @ 5%
    30000 + @ 10%
    
    For the second bucket, the max tax is $749.95

--- HW3/src/Problem_4.java
+++ HW3/src/Problem_4.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Problem_4 {
     public static void main(String[] args)
     {
-
+        float Income, Tax, MaxTax5p;
+        Tax = 0;
+        MaxTax5p = 749.95f;
     }
 }
