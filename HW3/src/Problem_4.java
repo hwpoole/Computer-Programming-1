9fbbdc76a46cc828306fa420aeeb90b9daca6a97
Hunter Poole <punterhoole@gmail.com>
2025-02-10 14:33:52 -0500

    H3: Add variable, fix logic issues
    Error in logic for second if statement, lines 25-29.
    
    Previously assessed a 5% tax on all income 0 - 30000.
    
    5% tax can only be assessed on any income between 15001 - 30000.
    
    Given the formatting of the 5% tax bracket (15001 - 30000), I have updated the 10% tax bracket from 30000 + to 30001 +

--- HW3/src/Problem_4.java
+++ HW3/src/Problem_4.java
@@ -4,33 +4,34 @@ import java.util.Scanner;
 public class Problem_4 {
     public static void main(String[] args)
     {
-        float Income, IncomeAbove30, Tax5p, MaxTax5p, Tax10p;
+        float Income, IncomeAbove15, IncomeAbove30, Tax5p, MaxTax5p, Tax10p;
 
         Scanner Input = new Scanner(System.in);
         System.out.print("Please enter the person's net income --> ");
         Income = Input.nextFloat();
 
-        IncomeAbove30 = Income - 30000f;
-        Tax5p = Income * 0.05f;
+        IncomeAbove15 = Income - 15001f;
+        IncomeAbove30 = Income - 30001f;
+        Tax5p = IncomeAbove15 * 0.05f;
         MaxTax5p = 749.95f;
         Tax10p = IncomeAbove30 * 0.10f;
 
         if (Income <= 15000)
         {
             System.out.println("Net Income = $" + Income);
-            System.out.print("State Tax = $0");
+            System.out.println("State Tax = $0");
         }
         
         if (Income >= 15001 && Income <= 30000)
         {
             System.out.println("Net Income = $" + Income);
-            System.out.print("State Tax = $" + Tax5p);
+            System.out.println("State Tax = $" + Tax5p);
         }
 
-        if (Income > 30000)
+        if (Income >= 30001)
         {
             System.out.println("Net Income = $" + Income);
-            System.out.print("State Tax = $" + (MaxTax5p + Tax10p));
+            System.out.println("State Tax = $" + (MaxTax5p + Tax10p));
         }
     }
 }
