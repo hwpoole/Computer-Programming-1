2011487eca99aa87a295e7caefef2ca927ebf02a
Hunter Poole <punterhoole@gmail.com>
2025-02-10 21:43:24 -0500

    H3: Add output and declaration

--- HW3/src/Problem_5.java
+++ HW3/src/Problem_5.java
@@ -1,3 +1,10 @@
+/*
+Name: Hunter Poole
+Date: 02/10/2025
+Homework # : 3
+Source File: Problem_5
+Action: Given a whole number, displays multiples of 3 down to 3.
+*/
 
 import java.util.Scanner;
 
@@ -19,3 +26,16 @@ public class Problem_5 {
         System.out.println();
     }
 }
+
+/*
+Provide your number: 16
+15 12 9 6 3 
+
+Provide your number: 25
+24 21 18 15 12 9 6 3 
+
+Provide your number: 70
+69 66 63 60 57 54 51 48 45 42 39 36 33 30 27 24 21 18 15 12 9 6 3 
+
+PS C:\Users\hunte\Documents\GitHub\155-W3-H3> 
+ */
