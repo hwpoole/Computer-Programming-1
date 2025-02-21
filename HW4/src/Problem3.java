b2c615662affa262581000773819dd065ee73336
Hunter Poole <punterhoole@gmail.com>
2025-02-20 19:10:03 -0500

    H4: Problem 3 Solution
    Problem 3 text:
    
    Write a program that has user
    enter a number between 1 and 7. If number is outside this range, display error message and end
    program. If number is within this range, then have the corresponding day displayed. Number 1
    will correspond to Sunday and 7 will be Saturday. Sample output below:
    
    Enter number 2
    Today is Monday
    
    For full credit, program must use a switch statement.

---
+++ HW4/src/Problem3.java
@@ -0,0 +1,83 @@
+/*
+Name: Hunter Poole
+Date: 2/20/25
+HW #: 4
+Problem #: 3
+Source Code: Problem3.java
+Action: Given a number 1 - 7, returns the current day for that number.
+        Sunday is 1.
+        Display error message and exit if number is not in range (1, 7)
+ */
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args)
+    {
+        int Day = 0;
+
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        Day = Input.nextInt();
+
+        if (Day < 1 || Day > 7)
+        {
+            System.out.println("Error: Please enter a number 1 - 7");
+            System.exit(0);
+        }
+
+        switch (Day)
+        {
+            case 1:
+                System.out.println("Today is Sunday");
+                break;
+            case 2:
+                System.out.println("Today is Monday");
+                break;
+            case 3:
+                System.out.println("Today is Tuesday");
+                break;
+            case 4:
+                System.out.println("Today is Wednesday");
+                break;
+            case 5:
+                System.out.println("Today is Thursday");
+                break;
+            case 6:
+                System.out.println("Today is Friday");
+                break;
+            case 7:
+                System.out.println("Today is Saturday");
+                break;
+        }
+    }
+}
+
+/*
+Enter a number 1
+Today is Sunday
+
+Enter a number 2
+Today is Monday
+
+Enter a number 3
+Today is Tuesday
+
+Enter a number 4
+Today is Wednesday
+
+Enter a number 5
+Today is Thursday
+
+Enter a number 6
+Today is Friday
+
+Enter a number 7
+Today is Saturday
+
+Enter a number 0
+Error: Please enter a number 1 - 7
+
+Enter a number 8
+Error: Please enter a number 1 - 7
+*/
