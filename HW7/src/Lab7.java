4c1febc9e9cc8a5b0b9f68b4962f7eeefe75ba4b
Hunter Poole <punterhoole@gmail.com>
2025-03-17 19:54:21 -0400

    H7: Set up Lab7 files

---
+++ HW7/src/Lab7.java
@@ -0,0 +1,58 @@
+/*
+Name: Hunter Poole
+Date: 3/17/25
+Lab #: 7
+Source Code: Lab7.java
+Action: 
+*/
+
+import java.util.Scanner;
+
+public class Lab7
+{
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void Plus(int N)
+    {
+
+    }
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void Minus(int N)
+    {
+
+    }
+
+    public static void main (String[] args)
+    {
+        int Num1, Num2;
+
+        Scanner Input = new  Scanner(System.in);
+
+        do
+        {
+            System.out.print("Enter your first integer --> ");
+            Num1 = Input.nextInt();
+
+            System.out.print("Enter your second integer --> ");
+            Num2 = Input.nextInt();
+
+        } while (Num1 != Num2)
+    }
+}
+
+/*
+ * 
+ */
