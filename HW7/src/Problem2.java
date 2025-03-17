37f22e07ad55ddb17c0d0779649335aa0c079c32
Hunter Poole <punterhoole@gmail.com>
2025-03-17 14:27:52 -0400

    H7: Problem 2 solution
    Problem 2 text:
    
    Program will have user enter an integer between 1 and 100, anything else
    will cause the loop and program to quit. This integer is first tested to see if it is an odd or
    even number; here an appropriate message is displayed, displayed only in the main
    function. Then the sum of all the numbers up to and including the entered number is
    calculated and displayed. So if the user enters the number 4, the sum of numbers from 1
    to 4 would yield 10. This program needs to call two separate methods, one to determine
    if number is odd or even, and the other to calculate the sum of the numbers. Both
    methods will have one parameter and both should return something back, which is the
    answer the main will deal with. Remember to design your functions so that they are
    logically coherent and follow the guidelines given in class and examples.

--- HW7/src/Problem2.java
+++ HW7/src/Problem2.java
@@ -13,15 +13,67 @@ public class Problem2
 {
 
 /*
-Action:
-Parameters:
-Returns:
-Precondition:
+Action: Determines if a number is odd or even
+Parameters: int N
+Returns: String " is even" || " is odd"
+Precondition: Input must be an integer
 */
  
+    static String OddOrEven (int N)
+    {
+        if (N % 2 == 0)
+        {
+            return " is even";
+        }
+        else
+        {
+            return " is odd";
+        }
+    }
+
+/*
+Action: Computes the sum of all integers up to and including
+        input integer, for starting integer 1
+Parameters: int N
+Returns: Sum of all integers from 1 to input integer, inclusive.
+Precondition: Input must be an integer. Starting range must be 1, inclusive.
+*/
+
+    static int SumOfInts (int N)
+    {
+        int Sum = (N * (1 + N)) / 2;
+        return Sum;
+    }
+//****************************************
     public static void main(String[] args)
     {
+        int Number;
+
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter an integer 1 - 100 --> ");
+        Number = Input.nextInt();
         
+        if (Number >= 1 && Number <= 100)
+        {
+            while (Number >= 1 && Number <= 100)
+            {
+                System.out.println(Number + OddOrEven(Number));
+                System.out.println("Sum of integers 1 - " + Number + " is: " + SumOfInts(Number));
+
+                System.out.print("Enter an integer 1 - 100 --> ");
+                Number = Input.nextInt();
+
+                if (Number < 1 || Number > 100)
+                {
+                    System.out.println("ERROR: Must enter an integer 1 - 100");
+                }
+            }
+        }
+        else
+        {
+            System.out.println("ERROR: Must enter an integer 1 - 100");
+        }
+
     }
 }
 
