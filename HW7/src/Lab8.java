07ec2dc06ce0d34f2c2a228b2555d8e5f67236d6
Hunter Poole <punterhoole@gmail.com>
2025-03-17 19:54:34 -0400

    H7: Set up Lab8 files

---
+++ HW7/src/Lab8.java
@@ -0,0 +1,46 @@
+/*
+Name: Hunter Poole
+Date: 3/17/25
+Lab #: 8
+Source Code: Lab8.java
+Action: 
+*/
+
+import java.util.Scanner;
+
+public class Lab8
+{
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void Func1()
+    {
+
+    }
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void Func2()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
+
+/*
+ * 
+ */
