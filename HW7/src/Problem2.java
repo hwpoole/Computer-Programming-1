1791e2eb1939631f2c781296c93e735e54379120
Hunter Poole <punterhoole@gmail.com>
2025-03-17 15:00:36 -0400

    H7: P2: Format output, provide test output

--- HW7/src/Problem2.java
+++ HW7/src/Problem2.java
@@ -4,7 +4,12 @@ Date: 3/17/25
 HW #: 7
 Problem #: 2
 Source Code: Problem2.java
-Action: 
+Action: Given an integer 1 - 100, determines if the integer
+        is odd or even via a method that returns to main.
+        Determines sum of integers of 1 - [Input] in the range
+        1 - 100, inclusive, via a method that returns to main.
+        Outputs result. Continues in a loop. Any input outside
+        of 1 - 100 will display an error and exit.
 */
 
 import java.util.Scanner;
@@ -58,7 +63,7 @@ Precondition: Input must be an integer. Starting range must be 1, inclusive.
             while (Number >= 1 && Number <= 100)
             {
                 System.out.println(Number + OddOrEven(Number));
-                System.out.println("Sum of integers 1 - " + Number + " is: " + SumOfInts(Number));
+                System.out.printf("%s %d %s %d %n%n","Sum of integers 1 -", Number, "is:", SumOfInts(Number));
 
                 System.out.print("Enter an integer 1 - 100 --> ");
                 Number = Input.nextInt();
@@ -78,5 +83,23 @@ Precondition: Input must be an integer. Starting range must be 1, inclusive.
 }
 
 /*
- * 
- */
+Enter an integer 1 - 100 --> 4
+4 is even
+Sum of integers 1 - 4 is: 10 
+
+Enter an integer 1 - 100 --> 12
+12 is even
+Sum of integers 1 - 12 is: 78 
+
+Enter an integer 1 - 100 --> 100
+100 is even
+Sum of integers 1 - 100 is: 5050 
+
+Enter an integer 1 - 100 --> 0              //In while-loop error
+ERROR: Must enter an integer 1 - 100
+*/
+
+/*
+Enter an integer 1 - 100 --> 101              //Before while-loop error
+ERROR: Must enter an integer 1 - 100
+*/
