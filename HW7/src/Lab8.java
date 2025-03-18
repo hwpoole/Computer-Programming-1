3416459ecc2a2e98dca7b3a306ef74b5aa07c61e
Hunter Poole <punterhoole@gmail.com>
2025-03-17 22:21:07 -0400

    H7: Lab 8 Solution
    Code comments included
    
    Write a program that will ask the user to enter a sentence. This sentence will then be
    passed into a function that will display how many blanks, or spaces, and how many
    vowels where in the sentence. It will also keep count of number of lower case letters. You
    can use any of the loop structures we have talked about. Don’t forget to use the logical
    “OR” and make use of the handouts as well.
    You will have only one function that will return nothing back, but display all data from
    inside this function.

--- HW7/src/Lab8.java
+++ HW7/src/Lab8.java
@@ -3,7 +3,9 @@ Name: Hunter Poole
 Date: 3/17/25
 Lab #: 8
 Source Code: Lab8.java
-Action: 
+Action: Asks for a sentence, then passes that sentence to a function.
+        That function will count spaces, lower case chars, and vowel chars.
+        Function will then print counts.
 */
 
 import java.util.Scanner;
@@ -12,35 +14,67 @@ public class Lab8
 {
 
 /*
-Action:
-Parameters:
-Returns:
-Precondition:
+Action: Takes a string, counts spaces, lower case chars and vowel chars. Prints counts.
+Parameters: String X
+Returns: void
+Precondition: A not-empty string needs to be provided.
 */
 
-    static void Func1()
+    static void StringPartsCalculator(String X)
     {
+        int SpaceCount = 0, LowerCaseCount = 0, VowelCount = 0;
+        String X_LowerCase;
 
-    }
+        X_LowerCase = X.toLowerCase();
 
-/*
-Action:
-Parameters:
-Returns:
-Precondition:
-*/
+        for (int i = 0; i < X.length(); i++)
+        {
+            if (Character.isWhitespace(X.charAt(i)))
+            {
+                SpaceCount++;
+            }
 
-    static void Func2()
-    {
+            if (Character.isLowerCase(X.charAt(i)))
+            {
+                LowerCaseCount++;
+            }
 
+            if (X_LowerCase.charAt(i) == 97 || X_LowerCase.charAt(i) == 101
+                || X_LowerCase.charAt(i) == 105 || X_LowerCase.charAt(i) == 111
+                || X_LowerCase.charAt(i) == 117)
+            {
+                VowelCount++;
+            }
+        }
+
+        System.out.printf("%n%s %d %n%s %d %n%s %d%n","Vowels =", VowelCount,
+                            "Spaces =", SpaceCount, "Lower case =", LowerCaseCount);
     }
 
     public static void main(String[] args)
     {
+        String Sentence;
 
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Please enter a sentence");
+        Sentence = Input.nextLine();
+
+        StringPartsCalculator(Sentence);
     }
 }
 
 /*
- * 
+Please enter a sentence
+This is a lovely Day.
+
+Vowels = 6 
+Spaces = 4 
+Lower case = 14
+
+Please enter a sentence
+This is my TRULY unique test tExt! !! 
+
+Vowels = 9 
+Spaces = 7 
+Lower case = 20
  */
