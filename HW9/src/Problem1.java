cf743401d5d40d82ef13cf3bc4d65ad5d0c12810
Hunter Poole <punterhoole@gmail.com>
2025-04-07 14:23:08 -0400

    H9: Initial setup

---
+++ HW9/src/Problem1.java
@@ -0,0 +1,31 @@
+/*
+Name: Hunter Poole
+Date:
+HW #:
+Problem #:
+Source Code:
+Action:
+*/
+
+import java.util.Scanner;
+
+public class Problem1
+{
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static int ReadInt (String X)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
