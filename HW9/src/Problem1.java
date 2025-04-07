0dc343346f55054e93687a33b91d334af2558187
Hunter Poole <punterhoole@gmail.com>
2025-04-07 16:18:44 -0400

    H9: Problem 1 framework
    Known answers coded, began exploring function logic to convert from string to integer.
    
    Need to figure out how to separate the string into an int without adding the integer values
    
    EX: 99 = (9 + 9) = 99
    NOT 99 = (9 + 9) = 18

--- HW9/src/Problem1.java
+++ HW9/src/Problem1.java
@@ -21,11 +21,47 @@ Precondition:
 
     static int ReadInt (String X)
     {
+        String Stack;
 
+        for (int i = 0; i <= X.length(); i++)
+        {
+            while (X.charAt(i) >= 48 || X.charAt(i) <= 57)
+            {
+                Stack = (X.charAt(i));
+            }
+        }
     }
 
     public static void main(String[] args)
     {
-        
+        String ProvidedString;
+        char Continue;
+        int ReturnedInt;
+
+        Scanner Input = new Scanner(System.in);
+
+        do 
+        {
+            System.out.print("Provide your string: ");
+            ProvidedString = Input.next();
+
+
+            if (ReturnedInt == -1)
+            {
+                System.out.print("ERROR -1");
+            }
+            if (ReturnedInt == -2)
+            {
+                System.out.print("ERROR -2");
+            }
+
+            System.out.print("Continue? Y or N: ");
+            Continue = Input.next().charAt(0);
+
+        } while (Continue != 'n' && Continue != 'N');
     }
 }
+
+/*
+ * 
+ */
