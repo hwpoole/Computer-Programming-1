cc80838a00ae695aec38b71e017bbd06e196e7de
Hunter Poole <punterhoole@gmail.com>
2025-04-16 13:57:48 -0400

    H10: P2 code comments and example output

--- HW10/src/Problem2.java
+++ HW10/src/Problem2.java
@@ -4,7 +4,8 @@ Date: 4/16/25
 HW #: 10
 Problem #: 2
 Source Code: Problem2.java
-Action:
+Action: Asks for a base and an Exponent from the user. Then, calls IntegerPower function
+        to calculate the result without using any of the math methods.
 */
 
 import java.util.Scanner;
@@ -13,10 +14,10 @@ public class Problem2
 {
 
 /*
-Action:
-Parameters:
-Returns:
-Precondition:
+Action: Given two integers, a base and an exponent, finds the result of the exponential math.
+Parameters: int Base, int Exponent
+Returns: int Result
+Precondition: int Exponent is non-zero and non-negative.
 */
 
     static int IntegerPower(int Base, int Exponent)
@@ -49,5 +50,35 @@ Precondition:
 }
 
 /*
+Please input the base --> 2
+Please input the exponenet --> 5
+Answer is = 32
 
+Please input the base --> 2
+Please input the exponenet --> 10
+Answer is = 1024
+
+Please input the base --> 3
+Please input the exponenet --> 3
+Answer is = 27
+
+Please input the base --> 8
+Please input the exponenet --> 2
+Answer is = 64
+*/
+
+// Constraints tests //
+
+/*
+Please input the base --> 2
+Please input the exponenet --> 0
+Answer is = 2
+
+Please input the base --> 2
+Please input the exponenet --> -1
+Answer is = 2
+
+Please input the base --> 2
+Please input the exponenet --> 999999999
+Answer is = 0
 */
