559efafa6f542c77cbad68ed05fe04fa46c0fa3a
Hunter Poole <punterhoole@gmail.com>
2025-04-16 17:08:24 -0400

    H10: Create problem 2

---
+++ HW10/src/Problem2.java
@@ -0,0 +1,33 @@
+/*
+Name: Hunter Poole
+Date: 4/16/25
+HW #: 10
+Problem #: 2
+Source Code: Problem2.java
+Action:
+*/
+
+public class Problem2
+{
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static int IntegerPower(int Base, int Exponent)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
+
+/*
+
+*/
