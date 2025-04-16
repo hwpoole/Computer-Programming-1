431f18bab0c64f5a8c22e8fca617a3d3e60ed6ef
Hunter Poole <punterhoole@gmail.com>
2025-04-16 13:27:59 -0400

    H10: Problem 2 Solution

--- HW10/src/Problem2.java
+++ HW10/src/Problem2.java
@@ -7,6 +7,8 @@ Source Code: Problem2.java
 Action:
 */
 
+import java.util.Scanner;
+
 public class Problem2
 {
 
@@ -19,12 +21,30 @@ Precondition:
 
     static int IntegerPower(int Base, int Exponent)
     {
+        int Result = Base;
 
+        for (int i = 2; i <= Exponent; i++)
+        {
+            Result *= Base;
+        }
+
+        return Result;
     }
 
     public static void main(String[] args)
     {
-        
+        int Base, Exponent, Answer;
+
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Please input the base --> ");
+        Base = Input.nextInt();
+
+        System.out.print("Please input the exponenet --> ");
+        Exponent = Input.nextInt();
+
+        Answer = IntegerPower(Base, Exponent);
+
+        System.out.print("Answer is = " + Answer);
     }
 }
 
