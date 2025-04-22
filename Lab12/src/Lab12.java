065b1ff91d22abc005beacfa6005064724bcb0c0
Hunter Poole <punterhoole@gmail.com>
2025-04-22 14:19:17 -0400

    H11: Lab12 setup

---
+++ Lab12/src/Lab12.java
@@ -0,0 +1,30 @@
+/*
+Name: Hunter Poole
+Date: 4/22/25
+Lab #: 12
+Source Code: Lab12.java
+Action:
+*/
+
+import java.util.Scanner;
+
+public class Lab12
+{
+    public static void main(String[] args)
+    {
+        String UserString;
+    }
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void FUNction()
+    {
+
+    }
+
+}
