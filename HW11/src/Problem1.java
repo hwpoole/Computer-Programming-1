ce262a3e8f919371b6d92c5477fd623c5fb009c1
Hunter Poole <punterhoole@gmail.com>
2025-04-21 20:00:56 -0400

    H11: Problem 1 Initial setup
    Code comments, main, three functions and their respective code comments

---
+++ HW11/src/Problem1.java
@@ -0,0 +1,59 @@
+/*
+Name: Hunter Poole
+Date: 4/21/25
+HW #: 11
+Problem #: 1
+Source Code: Problem1.java
+Action:
+*/
+
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void FillArray ()
+    {
+
+    }
+
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void FindAverage ()
+    {
+
+    }
+
+/*
+Action:
+Parameters:
+Returns:
+Precondition:
+*/
+
+    static void FindVariance ()
+    {
+
+    }
+
+}
+
+/*
+
+*/
